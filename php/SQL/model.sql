6dcba908034533f88d42ed64d90969c653035fba
Lulu VIALA <lulu.viala@isen-ouest.yncrea.fr>
2025-01-05 17:23:33 +0100

    SQL model fix

--- php/SQL/model.sql
+++ php/SQL/model.sql
@@ -7,49 +7,49 @@ DROP TABLE IF EXISTS Specialities CASCADE;
 DROP TABLE IF EXISTS Doctor_Jobs CASCADE;
 
 CREATE TABLE Client (
-    id SERIAL;
-    name VARCHAR (20) NOT NULL;
-    mail VARCHAR (40) NOT NULL;
-    telephone VARCHAR(12) NOT NULL;
-    password VARCHAR(40) NOT NULL;
+    id SERIAL,
+    name VARCHAR (20) NOT NULL,
+    mail VARCHAR (40) NOT NULL,
+    telephone VARCHAR(12) NOT NULL,
+    password VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE Appointement (
-    id SERIAL;
-    start DATE;
-    end DATE;
-    id_doctor INTEGER;
-    id_client INTEGER;
+    id SERIAL,
+    start DATE,
+    end DATE,
+    id_doctor INTEGER,
+    id_client INTEGER
 );
 
 CREATE TABLE Doctor (
-    id SERIAL;
-    name VARCHAR (20) NOT NULL;
-    mail VARCHAR (40) NOT NULL;
-    telephone VARCHAR (12) NOT NULL;
-    id_office INTEGER;
-    id_appointement INTEGER;
+    id SERIAL,
+    name VARCHAR (20) NOT NULL,
+    mail VARCHAR (40) NOT NULL,
+    telephone VARCHAR (12) NOT NULL,
+    id_office INTEGER,
+    id_appointement INTEGER,
     password VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE Office (
-    id SERIAL;
-    id_address INTEGER;
-    id_doctor INTEGER;
-    room_nbr INTEGER;
+    id SERIAL,
+    id_address INTEGER,
+    id_doctor INTEGER,
+    room_nbr INTEGER
 );
 
-CREATE TABLE Address () {
-    id SERIAL;
-    address VARCHAR (40) NOT NULL;
-}
+CREATE TABLE Address (
+    id SERIAL,
+    address VARCHAR (40) NOT NULL
+);
 
 CREATE TABLE Specialities (
-    id SERIAL;
+    id SERIAL,
     speciality_name VARCHAR (30) NOT NULL
 );
 
 CREATE TABLE Doctor_Jobs (
-    id_doctor INTEGER;
-    id_specialty INTEGER;
+    id_doctor INTEGER,
+    id_specialty INTEGER
 );
