4adc2ce7c81e55bdd6e4fa66bacbe24b3ae148d9
AW <achille.wagner@isen-ouest.yncrea.fr>
2025-01-07 15:39:41 +0100

    Accueil

--- html/Accueil.php
+++ html/Accueil.php
@@ -10,7 +10,7 @@
             //isConnected();
 
             include_once('../php/NavBar.php');
-            GetNavBar("Acceuil");
+            GetNavBar("Accueil");
         ?>
     </header>
 
@@ -19,18 +19,18 @@
             <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="icone_avatar.jpg" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Recherchez un Rendez Vous</h5>
-                    <p class="card-text">Prenez rendez vous avec le medecin sur notre platforme.</p>
-                    <a href="Recherche_RDV.php" class="btn btn-primary">Accedez</a>
+                    <h5 class="card-title">Recherchez un rendez-vous</h5>
+                    <p class="card-text">Prenez rendez-vous avec le médecin de votre choix sur notre platforme !</p>
+                    <a href="Recherche_RDV.php" class="btn btn-primary">RECHERCHER</a>
                 </div>
             </div>
 
             <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="websitelogo.png" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">Vos Rendez Vous Passez</h5>
-                    <p class="card-text">Prenez rendez vous avec les medecin que vous avez consulté précedament.</p>
-                    <a href="RDV_passe.php" class="btn btn-primary">Accedez</a>
+                    <h5 class="card-title">Vos rendez-vous passés</h5>
+                    <p class="card-text">Prenez rendez-vous avec les médecins que vous avez précédemment consulté.</p>
+                    <a href="RDV_passe.php" class="btn btn-primary">VOIR</a>
                 </div>
             </div>
         </div>
